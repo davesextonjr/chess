68169e622b7d138a1f7fbf26d664f75394152578
David Sexton Jr <davesextonjr@gmail.com>
2023-04-18 12:45:46 -0400

    add image object

---
+++ src/utilities/chessPieces.js
@@ -0,0 +1,14 @@
+export const chessPieceImages = {
+    b: "images/black-bishop.svg",
+    k: "images/black-king.svg",
+    n: "images/black-knight.svg",
+    p: "images/black-pawn.svg",
+    q: "images/black-queen.svg",
+    r: "images/black-rook.svg",
+    B: "images/white-bishop.svg",
+    K: "images/white-king.svg",
+    N: "images/white-knight.svg",
+    P: "images/white-pawn.svg",
+    Q: "images/white-queen.svg",
+    R: "images/white-rook.svg"
+}
