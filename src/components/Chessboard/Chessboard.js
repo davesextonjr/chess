a1ce02c9924ae0e09cdf73203498083cceeb8ddb
David Sexton Jr <davesextonjr@gmail.com>
2023-04-18 11:45:32 -0400

    Prepare to review

--- src/components/Chessboard/Chessboard.js
+++ src/components/Chessboard/Chessboard.js
@@ -9,16 +9,14 @@ export default function Chessboard(){
     //combine the arrays to make the chess board
     for(let i = 0; i < verticalAxis.length; i++){
         for(let j = 0; j< horizontalAxis.length; j++){
+
             //logic to determine whether the square is light or dark
             const number = i + j + 2
             const color = number % 2 !== 0 ? "dark-square" : "light-square"
             const name = horizontalAxis[j] + verticalAxis[i]
 
-            board.push(<Square color={color} name={name}>
-                {/* <div className={`${lightOrDark} ${horizontalAxis[j] + verticalAxis[i]}`}>{horizontalAxis[j]}{verticalAxis[i]}</div> */}
-            </Square>
-
-            )
+            //add each square to the board with its color and name
+            board.push(<Square color={color} name={name}/>)
         }
     }
 
--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -2,8 +2,8 @@ import './Square.css'
 
 export default function Square({color, name}){
     return(
-        <div className={`${color} ${name}`}>
-            {name}
+        <div className={`${color} ${name} square` }>
+            <img className='piece' src='images/black-bishop.svg'/>
         </div>
     )
 }
